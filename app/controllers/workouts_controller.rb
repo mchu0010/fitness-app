c671d4f3fd62bd813fab1e3238cd85f5b8ce54da
aamharris <aamharris@gmail.com>
2015-03-27 15:05:36 -0500

    create views for new and index

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,22 @@
 class WorkoutsController < ApplicationController
   def index
-     
+    @user = current_user
+    if logged_in?
+      render :'workout/index'
+    else
+      redirect_to root_path
+    end
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+    
   end
 end
