40b1e97039170ed909159e2797f0b178c9eb1896
aamharris <aamharris@gmail.com>
2015-03-28 14:53:44 -0500

    fix user and workout program association

--- db/migrate/20150328192223_create_user_workout_programs.rb
+++ db/migrate/20150328192223_create_user_workout_programs.rb
@@ -1,8 +1,8 @@
 class CreateUserWorkoutPrograms < ActiveRecord::Migration
   def change
     create_table :user_workout_programs do |t|
-      t.string :user_id
-      t.string :workout_program_id
+      t.integer :user_id
+      t.integer :workout_program_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328004341) do
+ActiveRecord::Schema.define(version: 20150328192223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,13 @@ ActiveRecord::Schema.define(version: 20150328004341) do
     t.integer  "second_user_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "user_workout_programs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "workout_program_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "users", force: :cascade do |t|
