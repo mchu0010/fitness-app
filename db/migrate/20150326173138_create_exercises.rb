ddcf1d3f757317e6baee647d485389be741d20c4
Michael Chu <m.chu90@gmail.com>
2015-03-27 22:32:01 -0500

    edit/delete attributes

--- db/migrate/20150326173138_create_exercises.rb
+++ db/migrate/20150326173138_create_exercises.rb
@@ -2,13 +2,12 @@ class CreateExercises < ActiveRecord::Migration
   def change
     create_table :exercises do |t|
     	t.string :name
-    	t.string :category
     	t.text :description
-    	t.string :machine
-    	t.string :primary_muscle
-    	t.string :secondary_muscle
+    	t.string :equipment
+    	t.references :muscle
     	t.string :image
     	t.text :video_link
+
       t.timestamps null: false
     end
   end
