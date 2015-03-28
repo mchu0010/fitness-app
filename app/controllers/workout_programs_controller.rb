ff31466799c1c3d2f9b7e5187767731f540eeadc
aamharris <aamharris@gmail.com>
2015-03-27 22:15:41 -0500

    add workout program view

--- app/controllers/workout_programs_controller.rb
+++ app/controllers/workout_programs_controller.rb
@@ -1,9 +1,13 @@
 class WorkoutProgramsController < ApplicationController
   def index
-
+    @workout_programs = WorkoutProgram.all
   end
 
   def show
-
+    @workout_program = WorkoutProgram.find(params[:id])
+    @schedule = DailyWorkout.where(workout_program_id: @workout_program.id)
+    @exercise = ExerciseRecommendation.where(daily_workout_id: @schedule)
   end
+
+
 end
