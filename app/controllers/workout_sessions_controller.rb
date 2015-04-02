2c4088e919c4e6d8eaf438e3bb165f897eca2784
bsheridan12 <bsheridan12@gmail.com>
2015-04-02 18:17:01 -0500

    work on setting routes and views

--- app/controllers/workout_sessions_controller.rb
+++ app/controllers/workout_sessions_controller.rb
@@ -2,6 +2,11 @@ class WorkoutSessionsController < ApplicationController
   def index
     @workout_session = WorkoutSession.create
 
+    @exercises = Exercise.order(:name)
+    @muscles = Muscle.order(:name)
+    @categories = MuscleGroup.all
+
+
     # need to create actiivity when exercise is chosen
     # @exercise_activity = ExerciseActivity.new(workout_session: @workout_session)
 
